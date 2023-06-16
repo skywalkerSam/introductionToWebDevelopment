41be83369919f988f4087da302fd0b629b18363c
Starboy <samskywalker001@gmail.com>
2023-06-16 11:12:04 +0530

    June16

---
+++ 19-React.js/robofriends-03/src/robots.js
@@ -0,0 +1,53 @@
+export const robots = [
+    {
+        id : 0, 
+        name : 'Robot-0', 
+        email : 'robot-0@skywalkersam.dev'
+    },
+    {
+        id : 1,
+        name : 'Robot-1',
+        email : 'robot-1@skywalkersam.dev'
+    },
+    {
+        id : 2,
+        name : 'Robot-2',
+        email : 'robot-2@skywalkersam.dev'
+    },
+    {
+        id : 3,
+        name : 'Robot-3',
+        email : 'robot-3@skywalkersam.dev'
+    },
+    {
+        id : 4,
+        name : 'Robot-4',
+        email : 'robot-4@skywalkersam.dev'
+    },
+    {
+        id : 5,
+        name : 'Robot-5',
+        email : 'robot-5@skywalkersam.dev'
+    },
+    {
+        id : 6,
+        name : 'Robot-6',
+        email : 'robot-6@skywalkersam.dev'
+    },
+    {
+        id : 7,
+        name : 'Robot-7',
+        email : 'robot-7@skywalkersam.dev'
+    },
+    {
+        id : 8,
+        name : 'Robot-8',
+        email : 'robot-8@skywalkersam.dev'
+    },
+    // {
+    //     id : 9,
+    //     name : 'Robot-9',
+    //     email : 'robot-9@skywalkersam.dev'
+    // },
+
+]; 
