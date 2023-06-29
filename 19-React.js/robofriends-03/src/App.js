57038ed53502b6e440315c0a13e12d8430716d27
Starboy <samskywalker001@gmail.com>
2023-06-29 21:32:24 +0530

    June29

--- 19-React.js/robofriends-03/src/App.js
+++ 19-React.js/robofriends-03/src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import CardList from "./CardList";
 import { robots } from './robots';
-
+import SearchBox from './SearchBox';
+import tachyons from 'tachyons';
  
-const App = () => {
-  return(
-    <div>
-      <h1>RoboFriends</h1>
-      <CardList robots={robots}/>
-    </div>
-  )
+class App extends Component {
+  constructor(){
+    super() 
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+    console.log(event.target.value);
+  }
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    })
+    
+    return(
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={filteredRobots}/>
+      </div>
+    );
+  }
 }
 
 export default App;
