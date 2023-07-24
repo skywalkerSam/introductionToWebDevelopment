99c8083e6fbd837dfe6f6da5b4bae02ea27eeeef
Starboy <samskywalker001@gmail.com>
2023-07-25 02:07:34 +0530

    July24

---
+++ 11-JavaScript/11-Arrow_Functions.js
@@ -0,0 +1,11 @@
+// Arrow Functions (Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions)
+
+const materials = [
+    'Hydrogen',
+    'Helium',
+    'Lithium',
+    'Beryllium'
+  ];
+  
+console.log(materials.map(material => material.length));
+// Expected output: Array [8, 6, 7, 9]
--- 11-JavaScript/Exercises/exercise9-Advanced_Control-Flow/exercise9.js
+++ 11-JavaScript/Exercises/exercise9-Advanced_Control-Flow/exercise9.js
@@ -33,12 +33,22 @@ function moveCommand(direction) {
     return whatHappens;
 }
 
+
 //#2 return value when moveCommand("forward");
+var whatHappens = moveCommand("forward") ? "you encounter a monster" : "please enter a valid direction"; 
 
 //#3 return value when moveCommand("back");
+var whatHappens = moveCommand("back") ? "you arrived home" : "please enter a valid direction";
 
 //#4 return value when moveCommand("right");
+var whatHappens = moveCommand("right") ? "you found a river" : "please enter a valid direction";
 
 //#5 return value when moveCommand("left");
+var whatHappens = moveCommand("left") ? "you run into a troll" : "please enter a valid direction";
 
 //BONUS: practice makes perfect. Go and write your own switch function. It takes time to get used to the syntax!
+
+
+// My Hot Take: Running JS code for testing, just Sucks!
+// 1. Browser Console  2. Node.js  
+// There's nothing like python interpreter, where you can just run the code and see the output!
