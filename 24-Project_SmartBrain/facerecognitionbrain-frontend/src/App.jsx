b45af0969f92c3d40d82ddd65852913d48adcd57
Starboy <samskywalker001@gmail.com>
2023-09-27 12:33:25 +0530

    Sept_27

--- 24-Project_SmartBrain/facerecognitionbrain-frontend/src/App.jsx
+++ 24-Project_SmartBrain/facerecognitionbrain-frontend/src/App.jsx
@@ -1,13 +1,21 @@
 import { Component } from "react";
+import './App.css'
+import ParticlesBg from "particles-bg";
 import { Logo } from "./components/Logo/Logo";
 import { Navigation } from "./components/Navigation/Navigation";
+import { ImageLinkForm } from "./components/ImageLinkForm/ImageLinkForm";
+import { Rank } from "./components/Rank/Rank";
 
 export default class App extends Component{
     render(){
         return(
-            <div>
+            <div className="App">
+                <ParticlesBg num={150} type="lines" bg={true} />
                 <Navigation></Navigation>
                 <Logo></Logo>
+                <Rank></Rank>
+                <ImageLinkForm></ImageLinkForm>
+
             </div>
         )
     }
--- 24-Project_SmartBrain/facerecognitionbrain-frontend/src/components/Navigation/Navigation.jsx
+++ 24-Project_SmartBrain/facerecognitionbrain-frontend/src/components/Navigation/Navigation.jsx
@@ -2,7 +2,7 @@ export function Navigation(){
     return(
         <div>
             <nav style={{display:"flex", justifyContent:"flex-end"}}>
-                <p className="f5 link dim underline pa3 pointer">Sign Out</p>
+                <p className="f5 link dim underline pa3 white pointer">Sign Out</p>
             </nav>
         </div>
     )
