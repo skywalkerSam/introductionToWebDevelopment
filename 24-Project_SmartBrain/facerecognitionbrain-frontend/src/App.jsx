8b862e29b32f63ff914b1fc16c62cbcac7f9b90f
Starboy <samskywalker001@gmail.com>
2023-10-16 03:37:33 +0530

    Oct_16

--- 24-Project_SmartBrain/facerecognitionbrain-frontend/src/App.jsx
+++ 24-Project_SmartBrain/facerecognitionbrain-frontend/src/App.jsx
@@ -25,8 +25,46 @@ export default class App extends Component{
     onButtonSubmit = () => {
         console.log('Fetching the Image... ')
         this.setState({imageUrl: this.state.input})
-        
-    }
+
+        const PAT = '46286541c4f34ed9b4f218f285777e05';
+        const USER_ID = 'skywalkersam';       
+        const APP_ID = 'facerecognitionbrain-frontend';
+        const MODEL_ID = 'face-detection';
+        const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
+        const IMAGE_URL = this.state.input;
+
+        const raw = JSON.stringify({
+            "user_app_id": {
+                "user_id": USER_ID,
+                "app_id": APP_ID
+            },
+            "inputs": [
+                {
+                    "data": {
+                        "image": {
+                            "url": IMAGE_URL
+                        }
+                    }
+                }
+            ]
+        });
+
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Key ' + PAT
+            },
+            body: raw
+        };
+
+        fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+            .then(response => response.json())
+            .then(result => console.log(result.outputs[0].data.regions[0].region_info.bounding_box))
+            .catch(error => console.log('error', error));
+            
+        }
+
 
     render(){
         return(
