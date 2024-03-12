a2b997635c663e7a88af85715eee9216450441bc
Starboy <samskywalker001@gmail.com>
2024-03-12 06:42:16 +0530

    Loops

---
+++ Introduction-to-React/basicReactConcepts/loops/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import 'tachyons'
+
+
+function App() {
+
+  return (
+    <div className='mr3 grey'>
+      <div>
+        <h1>Loops</h1>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
