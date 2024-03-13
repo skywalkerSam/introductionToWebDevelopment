e1bcd755abaee1916a61186e60f9a6f2feb1207b
Starboy <samskywalker001@gmail.com>
2024-03-13 14:26:23 +0530

    Lifecycle and Effects

---
+++ Introduction-to-React/basicReactConcepts/Lifecycle-and-Effects/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import 'tachyons'
+import React, { useState, useEffect } from 'react'
+
+
+function App() {
+
+  return (
+    <div className='mr3 grey'>
+
+      <div>
+
+        <h1 className='green'>Lifecycle and Effects</h1>
+        <hr />
+      </div>
+
+     
+      <div className='mt4'>
+       
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
