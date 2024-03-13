767bab89ff4dca1db03993d28f4a874156b43e88
Starboy <samskywalker001@gmail.com>
2024-03-13 06:54:56 +0530

    React State

---
+++ Introduction-to-React/basicReactConcepts/reactState/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import 'tachyons'
+import React, { useState } from 'react'
+
+
+
+
+function App() {
+  
+
+  return (
+    <div className='mr3 grey'>
+
+      <div>
+
+        <h1 className='blue'>React State</h1>
+        <hr />
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default App
