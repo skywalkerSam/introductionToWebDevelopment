f85b1b242662d3b2ace71302072c4ee477870bde
Starboy <samskywalker001@gmail.com>
2024-04-03 16:40:03 +0530

    Node + Express + TS

---
+++ My-Projects/animal-farm/server/src/index.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import Chance from "chance";
+
+const PORT = 8080;
+
+// Initialize the express app
+const app = express();
+app.use(cors());
+app.use(express.json()); // json body parser
+
+// Make some animals
+const chance = new Chance();
+
+// JS doesn't have range by default, so...
+const animals = [...Array(250).keys()].map((id) => {
+  return {
+    id,
+    type: chance.animal(),
+    age: chance.age(),
+    name: chance.name(),
+  };
+});
+
+// GET endpoint to search for animals
+app.get("/animals", (req, res) => {
+  const q = req.query.q?.toLowerCase().trim() || "";
+  const results = animals.filter((animal) =>
+    animal.type.toLowerCase().includes(q)
+  );
+
+  res.send(results);
+});
+
+// Start the server
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
