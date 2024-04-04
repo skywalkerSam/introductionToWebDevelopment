e1da82f6a37d25ce3257fb9c8a94b46bab9a1c67
Starboy <samskywalker001@gmail.com>
2024-04-05 00:59:08 +0530

    Animals update

--- My-Projects/animal-farm/server/src/index.ts
+++ My-Projects/animal-farm/server/src/index.ts
@@ -34,7 +34,7 @@ app.get('/animals', (req, res) => {
 
 // Start the server
 app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`Server running on http://localhost:${PORT}/animals/`)
 );
 
 
