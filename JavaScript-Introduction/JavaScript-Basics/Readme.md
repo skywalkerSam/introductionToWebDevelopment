815f0218ed43b41363d025704214fdb525003bff
Starboy <samskywalker001@gmail.com>
2024-04-26 14:11:18 +0530

    Review

---
+++ JavaScript-Introduction/JavaScript-Basics/Readme.md
@@ -0,0 +1,1 @@
+# [Javascript Crash Course](https://fireship.io/courses/js/101-basics/)
--- JavaScript-Introduction/JavaScript-Basics/variables.js
+++ JavaScript-Introduction/JavaScript-Basics/variables.js
@@ -1,6 +1,6 @@
 // Using camelCase for variable names is a good practice in JavaScript.
 
-// var
+// var, NEVER use it...!
 var userName = "skywalkerSam";
 
 // let
@@ -15,3 +15,38 @@ console.log(userPlanet);
 
 // I think that, semi-colons(;) are just a convention in Javascript. Big deal in C/C++ & Completely optional in python... lol ;)
 // ASI - Automatic Semicolon Insertion
+
+
+/* Scopes
+
+Global Scope (let, const, var)
+Local Scope, Function Scope ( var)
+Block Scope ( let, const)
+
+*/
+
+// Global/Root/Window Scope
+var scopeType = "Global";   // mutable
+
+// Local Scope
+function localScope() {
+    // Child scope
+    var scopeType = "Local";
+    console.log(scopeType);
+
+    // if you don't use `var` here, it will replace the global variable...!
+}
+
+// Block Scope
+function blockScope(params) {
+    for (let i = 0; i < 3; i++) {
+        console.log(i, params);
+    }
+}
+
+
+blockScope(scopeType);  // 0 "Global" 1 "Global" 2 "Global"
+
+localScope();   // Local
+
+console.log(scopeType); 
