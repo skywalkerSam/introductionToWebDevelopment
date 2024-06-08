f955e4334551013939a0a21f891636bd62c15406
Starboy <samskywalker001@gmail.com>
2024-06-09 00:19:17 +0530

    initials

---
+++ myProjects/theDream/theVanillaProject/vite.config.ts
@@ -0,0 +1,35 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'theVanillaProject',
+      short_name: 'theVanilla',
+      description: 'Text to Image with DALLE 3 using Vanilla.js',
+      theme_color: '#000000',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
