738dbfbaadaf5279624a859879f43d2e89deeb35
Starboy <samskywalker001@gmail.com>
2024-06-11 14:12:46 +0530

    fix: error handling

--- myProjects/theDream/theDreamProject/server/src/index.ts
+++ myProjects/theDream/theDreamProject/server/src/index.ts
@@ -18,39 +18,61 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Image Generation API...");
 });
 
-app.post("/test", (req, res) => {
-  const prompt = req.body.prompt;
-  const imageUrl =
-    "https://cdna.artstation.com/p/assets/images/images/076/708/342/4k/luis-omar-jinxrender-0010.jpg?1717601087";
-
-  console.log("\n...Incoming Request: ", prompt);
-
-  // mimic API response: add delay
-  function sleep(time, callback) {
-    const stop = new Date().getTime();
-    while (new Date().getTime() < stop + time) {
-      console.log("Intentional delay...\n");
-    }
-    callback();
+function sleep(time, callback) {
+  const stop = new Date().getTime();
+  while (new Date().getTime() < stop + time) {
+    console.log("Intentional delay...\n");
   }
+  callback();
+}
 
-  sleep(5000, function () {
-    res.send({ imageUrl });
-  });
+app.post("/test", (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    const imageUrl =
+      "https://cdna.artstation.com/p/assets/images/images/076/708/342/4k/luis-omar-jinxrender-0010.jpg?1717601087";
+
+    console.log("\n...Incoming Request: ", prompt);
+
+    // mimic API response: add delay
+    sleep(5000, function () {
+      res.send({ imageUrl });
+    });
+  } catch (error) {
+    if (error.response) {
+      res.send(error.response.status);
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      res.send(error.message);
+      console.log(error.message);
+    }
+  }
 });
 
 app.post("/image", async (req, res) => {
-  const prompt = req.body.prompt;
+  try {
+    const prompt = req.body.prompt;
 
-  const response = await openai.images.generate({
-    model: "dall-e-3",
-    prompt: prompt ?? "a starboy",
-    n: 1,
-    size: "1024x1024",
-  });
-  const imageUrl = response.data[0].url;
-  console.log(imageUrl);
-  res.send({ imageUrl });
+    const response = await openai.images.generate({
+      model: "dall-e-3",
+      prompt: prompt ?? "a starboy",
+      n: 1,
+      size: "1024x1024",
+    });
+    const imageUrl = response.data[0].url;
+    console.log(imageUrl);
+    res.send({ imageUrl });
+  } catch (error) {
+    if (error.response) {
+      res.send(error.response.status);
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      res.send(error.message);
+      console.log(error.message);
+    }
+  }
 });
 
 app.listen(process.env.PORT, () => {
