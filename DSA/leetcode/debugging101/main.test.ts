b0e83056e4a014b5172bc0e2579bfa06516a9379
Starboy <samskywalker001@gmail.com>
2024-07-12 20:19:41 +0530

    initials

---
+++ DSA/leetcode/debugging101/main.test.ts
@@ -0,0 +1,18 @@
+import { expect, test } from "vitest";
+
+let array: string[] = ["hello", "world", "wtf", "fml", "idk"];
+
+let numArray: number[] = [1, 3, 3, 3, 6, 9, 9, 6, 3, 1];
+
+let someArray: number[] | string[] = ["hello", "world"];
+
+let anotherArray: number[] | string[] = [3, 2, 1];
+
+// EXAMPLE:
+
+// import { something } from './src/index.ts';
+// test('Binary Search', () => {
+//     expect(functionName(someArray, 'some string')).toBe(0)
+// })
+
+// WRITE TESTS HERE:
