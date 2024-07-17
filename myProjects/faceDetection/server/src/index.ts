859f29b3fdaac56f78620dfafcdb77bff094ceb8
Starboy <samskywalker001@gmail.com>
2024-07-18 05:17:40 +0530

    initials

--- myProjects/faceDetection/server/src/index.ts
+++ myProjects/faceDetection/server/src/index.ts
@@ -1,47 +1,65 @@
 import express from "express";
 import cors from "cors";
-import Chance from "chance";
+import knex from "knex";
+import { handleSignIn } from "./controllers/signIn.ts";
+import { handleSignUp } from "./controllers/signUp.ts";
+import { getProfile } from "./controllers/profile.ts";
+import { detectFace, handleImage } from "./controllers/faceDetection.ts";
+import { getAnimals } from "./controllers/animals.ts";
 
-const PORT: number = 8080;
+const PORT = process.env.PORT || 8080;
 
-// Initialize the express app
+const db = knex({
+  client: "pg",
+  connection: {
+    database: process.env.DB,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || "5432", 10),
+    user: process.env.DB_USER,
+    password: process.env.DB_USER,
+  },
+});
+
+// initialize server
 const app = express();
 app.use(cors());
 app.use(express.json()); // json body parser
 
-// Make some animals
-const chance = new Chance();
-
-// JS doesn't have range by default, so...
-const animals = [...Array(250).keys()].map((id) => {
-  return {
-    id,
-    type: chance.animal(),
-    age: chance.age(),
-    name: chance.name(),
-  };
+app.get("/", (req, res) => {
+  res.json("Welcome to the Face Detection API...");
 });
 
-app.get('/', (req, res) => {
-  res.send("Welcome to Starboy Farms Inc.")
-})
-
-// GET endpoint to search for animals
-app.get('/animals', (req, res) => {
-  const q = req.query.q?.toLowerCase().trim() || '';
-  const results = animals.filter((animal) =>
-    animal.type.toLowerCase().includes(q)
-  );
-
-  res.send(results);
+app.post("/signin", (req, res) => {
+  handleSignIn(req, res, db);
+});
+app.post("/signup", (req, res) => {
+  handleSignUp(req, res, db);
+});
+app.get("/profile/:userId", (req, res) => {
+  getProfile(req, res, db);
+});
+app.put("/image", (req, res) => {
+  handleImage(req, res, db);
+});
+app.post("/imageurl", (req, res) => {
+  detectFace(req, res);
+});
+app.get("/animals", (req, res) => {
+  getAnimals(req, res);
 });
 
-// Start the server
+// start server
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}/animals/`)
 );
 
 
-// Query example... http://localhost:8080/animals/?q=horse
+// Error with the grpc module
 
-// npm run build ; node .
+// node_modules/@grpc/grpc-js/node_modules/long/umd/index.d.ts:1:18 - error TS1479: The current file is a CommonJS module whose imports will produce 'require' calls; however, the referenced file is an ECMAScript module and cannot be imported with 'require'. Consider writing a dynamic 'import("../index.js")' call instead.
+
+// 1 import Long from "../index.js";
+//                    ~~~~~~~~~~~~~
+
+
+// Found 1 error in node_modules/@grpc/grpc-js/node_modules/long/umd/index.d.ts:1
