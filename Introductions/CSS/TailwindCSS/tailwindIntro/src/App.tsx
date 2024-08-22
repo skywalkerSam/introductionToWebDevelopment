7ebe3cbf323f340cf943766d6dd4cc54e4111867
Starboy <samskywalker001@gmail.com>
2024-08-22 23:30:35 +0530

    tailwindcss intro

---
+++ Introductions/CSS/TailwindCSS/tailwindIntro/src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import appLogo from "/favicon.svg";
+import PWABadge from "./PWABadge.tsx";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img
+            src={appLogo}
+            className="logo inline-block"
+            alt="Tailwind CSS Introduction logo"
+          />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img
+            src={reactLogo}
+            className="logo react inline-block"
+            alt="React logo"
+          />
+        </a>
+      </div>
+      <h1 className="font-sans">Tailwind CSS Introduction</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <PWABadge />
+    </>
+  );
+}
+
+export default App;
