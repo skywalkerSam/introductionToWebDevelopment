39273d3a308d0e421d0eeb615206a370a2e61ab2
Starboy <samskywalker001@gmail.com>
2024-09-09 18:33:38 +0530

    shadcn-ui

--- tests/shadcn-ui/shadcn-ui-tests/src/App.tsx
+++ tests/shadcn-ui/shadcn-ui-tests/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
-import PWABadge from './PWABadge.tsx'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import appLogo from "/favicon.svg";
+import PWABadge from "./PWABadge.tsx";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -31,7 +31,7 @@ function App() {
       </p>
       <PWABadge />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
