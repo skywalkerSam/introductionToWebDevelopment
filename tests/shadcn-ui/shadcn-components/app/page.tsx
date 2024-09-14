c01f9e66b97ad31fbcdc3ee089cab45c01f483c9
Starboy <samskywalker001@gmail.com>
2024-09-14 13:36:15 +0530

    shadcn-next

--- tests/shadcn-ui/shadcn-components/app/page.tsx
+++ tests/shadcn-ui/shadcn-components/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
@@ -12,6 +17,7 @@ export default function Home() {
           height={38}
           priority
         />
+
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
