94296b7d2bb6f7649de8483a159e159bd394df93
Starboy <samskywalker001@gmail.com>
2024-09-18 18:10:05 +0530

    next basics

---
+++ Frontend/NEXT/notes-app/app/notes/[id]/error.tsx
@@ -0,0 +1,9 @@
+export default function Error() {
+  return (
+    <div>
+      <p className="text-red-600 font-semibold">
+        Something went wrong, try again!
+      </p>
+    </div>
+  );
+}
---
+++ Frontend/NEXT/notes-app/app/notes/[id]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div>
+      <p className="text-green-600 font-semibold">Please wait, Loading...</p>
+    </div>
+  );
+}
